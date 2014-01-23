c0935d82cd0f4e5934f4c279e2d690963eba33b4
Meiwin Fu <meiwin.fu@thestakeholdercompany.com>
2014-01-23 10:17:41 +0800

    Initial Commit

---
+++ ISVerticalMenuView/ISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ISAppDelegate.h
+//  ISVerticalMenuView
+//
+//  Created by Meiwin TSC on 23/1/14.
+//  Copyright (c) 2014 The Stakeholder Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ISVerticalMenuView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ISVerticalMenuView
+//
+//  Created by Meiwin TSC on 23/1/14.
+//  Copyright (c) 2014 The Stakeholder Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ISAppDelegate class]));
+    }
+}
